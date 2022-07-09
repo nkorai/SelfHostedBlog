32fbee043f26f33a124b23d1e99bb84d3f4ed03c
nkorai <nausherwan.korai@gmail.com>
2022-07-09 09:22:49 -0400

    Refactoring build artifacts into build folder, still trying to figure secret situation out

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker ps -all -q --filter "name=rproxy" | grep -q . && docker stop rproxy && docker rm -fv rproxy
-rm -rf nginx
-rm -rf ssl
+rm -rf build
 ls
--- bin/run.sh
+++ bin/run.sh
@@ -6,24 +6,23 @@ eval "./bin/clean.sh"
 # Hack to get around bind mounting nginx in the compose file
 # https://github.com/nginxinc/docker-nginx/issues/360
 docker container create --name tmp_nginx nginx
-mkdir -p ./nginx/conf/
-mkdir -p ./ssl/private/
-docker cp tmp_nginx:/etc/nginx/ ./nginx/conf/
-docker cp tmp_nginx:/etc/ssl/private ./ssl/private/
+mkdir -p ./build/nginx/conf/
+mkdir -p ./build/ssl/private/
+docker cp tmp_nginx:/etc/nginx/ ./build/nginx/conf/
+docker cp tmp_nginx:/etc/ssl/private ./build/ssl/private/
 docker container rm tmp_nginx
 
 # Create nginx folders
-mkdir -p ./nginx/conf/nginx/conf.d/sites-available
-mkdir -p ./nginx/conf/nginx/conf.d/sites-enabled
+mkdir -p ./build/nginx/conf/nginx/conf.d/sites-available
+mkdir -p ./build/nginx/conf/nginx/conf.d/sites-enabled
 
 # Copy over new nginx configuration
-# TODO: overkill and symlink these in the future
-cp bin/nginx/new_default.conf ./nginx/conf/nginx/conf.d/sites-available
-cp bin/nginx/new_default.conf ./nginx/conf/nginx/conf.d/sites-enabled
-rm ./nginx/conf/nginx/conf.d/default.conf
+cp bin/nginx/new_default.conf ./build/nginx/conf/nginx/conf.d/sites-available
+cp bin/nginx/new_default.conf ./build/nginx/conf/nginx/conf.d/sites-enabled
+rm ./build/nginx/conf/nginx/conf.d/default.conf
 
 # Point nginx configuration to our custom new default configuration
-sed -i 's|include /etc/nginx/conf.d/\*.conf;|include /etc/nginx/conf.d/sites-enabled/\*.conf;|g' ./nginx/conf/nginx/nginx.conf
+sed -i 's|include /etc/nginx/conf.d/\*.conf;|include /etc/nginx/conf.d/sites-enabled/\*.conf;|g' ./build/nginx/conf/nginx/nginx.conf
 
 # Ready to bring up our services
 docker-compose up
