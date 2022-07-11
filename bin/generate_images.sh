53477759fa2f4f7b2d70b28f47897c98306b8f6d
nkorai <nausherwan.korai@gmail.com>
2022-07-10 20:59:32 -0400

    Added in docker-compose.yaml vizualization

---
+++ bin/generate_images.sh
@@ -0,0 +1,2 @@
+docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image docker-compose.yaml --no-volumes --force --output-file=docker-compose-no-volumes.png
+docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image docker-compose.yaml --force --output-file=docker-compose-volumes.png
