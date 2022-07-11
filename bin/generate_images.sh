64fd55eb6b5e25c222fdd82dd81b0e9d093697d5
nkorai <nausherwan.korai@gmail.com>
2022-07-10 21:00:39 -0400

    Add credit

--- bin/generate_images.sh
+++ bin/generate_images.sh
@@ -1,2 +1,3 @@
+# Via https://github.com/pmsipilot/docker-compose-viz
 docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image docker-compose.yaml --no-volumes --force --output-file=docker-compose-no-volumes.png
 docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image docker-compose.yaml --force --output-file=docker-compose-volumes.png
