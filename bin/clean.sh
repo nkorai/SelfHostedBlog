f478d04acea7e6b8684d6b80c2830b51c728d339
nkorai <nausherwan.korai@gmail.com>
2023-01-15 13:41:52 -0500

    Add Ghost to restart/delete script to ensure automation is set up correctly

--- bin/clean.sh
+++ bin/clean.sh
@@ -2,4 +2,5 @@
 docker ps -all -q --filter "name=rproxy" | grep -q . && docker stop rproxy && docker rm rproxy
 docker ps -all -q --filter "name=ddns-updater" | grep -q . && docker stop ddns-updater && docker rm ddns-updater
 docker ps -all -q --filter "name=letsencrypt" | grep -q . && docker stop letsencrypt && docker rm letsencrypt
+docker ps -all -q --filter "name=ghost" | grep -q . && docker stop ghost && docker rm ghost
 rm -rf build
